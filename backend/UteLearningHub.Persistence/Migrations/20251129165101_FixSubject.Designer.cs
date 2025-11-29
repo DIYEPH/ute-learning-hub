b1abf0a05c7970ee44e934040c7f90c813b66724
DIYEPH <dinhyenphan@gmail.com>
2025-11-29 23:52:42 +0700

    [UTE_HUB - 67] Migration: Fix Subject

--- backend/UteLearningHub.Persistence/Migrations/20251129165101_FixSubject.Designer.cs
+++ backend/UteLearningHub.Persistence/Migrations/20251129165101_FixSubject.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using UteLearningHub.Persistence;
 
@@ -11,9 +12,11 @@ using UteLearningHub.Persistence;
 namespace UteLearningHub.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251129165101_FixSubject")]
+    partial class FixSubject
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/UteLearningHub.Persistence/Migrations/20251129165101_FixSubject.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UteLearningHub.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixSubject : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "NganhId",
+                table: "MonHoc",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Logo",
+                table: "Khoa",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Logo",
+                table: "Khoa");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "NganhId",
+                table: "MonHoc",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+        }
+    }
+}
