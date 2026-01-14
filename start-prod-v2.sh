36d359a34c91b5227ad63d3b4932b95d6aee3f70
DIYEPH <dinhyenphan@gmail.com>
2026-01-15 01:12:45 +0700

    [UTE_HUB - 144]: Fix start-prod-v2.sh

--- start-prod-v2.sh
+++ start-prod-v2.sh
@@ -3,16 +3,13 @@
 
 echo "--> Starting UTE Learning Hub (Production)..."
 
-COMPOSE_FILES="-f docker-compose.yml -f docker-compose.prod.yml"
-ENV_FILE="--env-file .env.production"
-
 if [ ! -f ".env.production" ]; then
     echo "[ERROR] .env.production not found!"
     exit 1
 fi
 
-docker compose $ENV_FILE $COMPOSE_FILES build --parallel
-docker compose $ENV_FILE $COMPOSE_FILES up -d backend frontend ai nginx
+docker compose -f docker-compose.yml -f docker-compose.prod.yml --env-file .env.production build --parallel
+docker compose -f docker-compose.yml -f docker-compose.prod.yml --env-file .env.production up -d backend frontend ai nginx
 
 echo "--> Services started!"
-docker compose $ENV_FILE $COMPOSE_FILES ps
+docker compose -f docker-compose.yml -f docker-compose.prod.yml --env-file .env.production ps
